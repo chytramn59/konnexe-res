f1477d1dadddd408d984bfe58917df7fafa49931
chytramn59 <cnagaraju@qwinix.io>
2018-05-17 18:11:28 +0530

    hi

--- js/login.js
+++ js/login.js
@@ -1,28 +1,27 @@
-$(document).ready(function() {
-  $('#email-check').on('input', function() {
-    var input=$('#email-check').val();
-    var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if(!re.test(input)){
-      $('#email-error').removeClass('hide');/* $('#fn').css('border-color','red');*/
-    }
-    else{
-      $('#email-error').addClass('hide');
-    }
-    
 
-   });
-    $("#password-check").keypress(function(){
-    var pass = $('#password-check').val();
-    var nameregex = /^[a-zA-Z0-9.@#$]{5,8}$/;
-    if(!nameregex.test(pass)){
-      $('#password-error').removeClass('hide');/* $('#fn').css('border-color','red');*/
-    }
-    else{
-      $('#password-error').addClass('hide');
+$(function() {
+  
+  $("form[name='registration']").validate({
+    
+    rules: {
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 5
+      }
+    },
+    messages: {
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long"
+      },
+      email: "Please enter a valid email address"
+    },
+    submitHandler: function(form) {
+      form.submit();
     }
   });
-  $("#submit").click(function(){
-    alert("please fill all the fields.");
-  }); 
-
 });
